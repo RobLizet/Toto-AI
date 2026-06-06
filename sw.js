bd2fba14f31b3f2227be7ac544b41332cbce097f
RobLizet <zweetzakken@gmail.com>
2026-06-06 08:53:44 +0200

    sw 35.5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ProMatchXI Service Worker v25.2 — Cache bust bij elke deploy
-const SW_VERSION = '35.4';
+const SW_VERSION = '35.5';
 const CACHE = 'totoai-' + SW_VERSION;
 
 self.addEventListener('install', e => { self.skipWaiting(); });
